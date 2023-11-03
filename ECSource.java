6aa100e4303e63070019b003b0aa55ba098988ac
Herman <julian.herman@fiserv.com>
2023-11-03 13:37:57 -0400

    implemented iterative solutions

--- ECSource.java
+++ ECSource.java
@@ -1,29 +1,36 @@
-class Node<T> {
-  T val;
-  Node<T> next;
-  
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-
 class ECSource {
-  public static <T> T getNodeValue(Node<T> head, int index) {
-    // todo - note the return type, but don't overthink it
-    return;
-  }
-  
-  public static void main(String[] args) {
-    Node<String> node1 = new Node<>("banana");
-    Node<String> node2 = new Node<>("mango");
-    Node<String> node3 = new Node<>("kiwi");
+    public static <T> T getNodeValue(Node<T> head, int index) {
+        // todo - note the return type, but don't overthink it
+        int count = 0;
+        while ( count < index && head != null ) {
+            head = head.next;
+            count++;
+        }
+        if ( head == null ) return null;
+        return head.val;
+    }
 
-    node1.next = node2;
-    node2.next = node3;
+    public static void main(String[] args) {
+        System.out.println("Test 1");
+        Node<String> node1 = new Node<>("banana");
+        Node<String> node2 = new Node<>("mango");
+        Node<String> node3 = new Node<>("kiwi");
+        node1.next = node2;
+        node2.next = node3;
+        // banana -> mango -> kiwi
+        System.out.println(ECSource.getNodeValue(node1, 0));
+        System.out.println(ECSource.getNodeValue(node1, 2));
 
-    // banana -> mango -> kiwi
 
-    System.out.println(ECSource.getNodeValue(node1, 1));
-  }
+        System.out.println("\nTest 2");
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        // a -> b -> c -> d
+        System.out.println(ECSource.getNodeValue(a, 7)); // null
+    }
 }
--- Source.java
+++ Source.java
@@ -1,33 +1,41 @@
 class Node<T> {
     T val;
     Node<T> next;
-    
+
     public Node(T val) {
-      this.val = val;
-      this.next = null;
+        this.val = val;
+        this.next = null;
     }
-  }
-  
-  class Source {
+}
+
+class Source {
     public static <T> boolean linkedListFind(Node<T> head, T target) {
-      // todo
-      return false;
+        boolean result = false;
+        while ( head != null ) {
+            if ( head.val.equals(target) ) {
+                result = true;
+                break;
+            }
+            head = head.next;
+        }
+        return result;
     }
-    
+
     public static void main(String[] args) {
+        System.out.println("Test 1");
         Node<String> a = new Node<>("a");
         Node<String> b = new Node<>("b");
         Node<String> c = new Node<>("c");
         Node<String> d = new Node<>("d");
-
         a.next = b;
         b.next = c;
         c.next = d;
-
         // a -> b -> c -> d
-
         System.out.println(Source.linkedListFind(a, "c"));
-        
-    }
 
-  }
+        System.out.println("\nTest 2");
+        Node<Integer> node1 = new Node<>(42);
+        // 42
+        System.out.println(Source.linkedListFind(node1, 100)); // false
+    }
+}
